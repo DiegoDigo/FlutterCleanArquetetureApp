39ae645298fdefe0ef7073b9f64dc812a1ada052
Diego Delmiro <diego.delmiro@amcom.com.br>
2022-01-17 16:59:43 -0300

    adicionando teste de 400 a 500

--- lib/data/remote/use_case/remote_authentication.dart
+++ lib/data/remote/use_case/remote_authentication.dart
@@ -1,5 +1,6 @@
-import '../http/http.dart';
+import '../../../domain/helper/help.dart';
 import '../../remote/dto/dtos.dart';
+import '../http/http.dart';
 
 class RemoteAuthentication {
   final HttpClient httpClient;
@@ -9,7 +10,13 @@ class RemoteAuthentication {
 
   Future<void> auth(AuthenticationParams params) async {
     final body = RemoteAuthenticationParams.fromEntity(params).toJson();
-    await httpClient.request(url: url, method: 'post', body: body);
+    try {
+      await httpClient.request(url: url, method: 'post', body: body);
+    } on HttpError catch (error) {
+      throw error == HttpError.unauthorized
+          ? DomainError.invalidCredentials
+          : DomainError.unexpected;
+    }
   }
 }
 
--- test/data/usecase/remote_authenticatio_teste.dart
+++ test/data/usecase/remote_authenticatio_teste.dart
@@ -1,5 +1,6 @@
 import 'package:clean_flutter_app/data/remote/http/http.dart';
 import 'package:clean_flutter_app/data/remote/use_case/remote_authentication.dart';
+import 'package:clean_flutter_app/domain/helper/help.dart';
 import 'package:faker/faker.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
@@ -14,6 +15,7 @@ void main() {
   late String email;
   late String password;
   late RemoteAuthentication sut;
+  late AuthenticationParams params;
 
   setUp(() {
     httpClient = MockHttpClient();
@@ -21,6 +23,7 @@ void main() {
     sut = RemoteAuthentication(httpClient: httpClient, url: url);
     email = faker.internet.email();
     password = faker.internet.password();
+    params = AuthenticationParams(email: email, password: password);
   });
 
   test('Should call httpClient with correct values', () async {
@@ -28,5 +31,37 @@ void main() {
     await sut.auth(params);
     verify(httpClient.request(url: url, method: 'post', body: params.toJson()))
         .called(1);
+  });
+
+  test('Should throw unexpected if HttpClient return 400 ', () {
+    when(httpClient.request(url: url, method: "post", body: params.toJson()))
+        .thenThrow(HttpError.badRequest);
+
+    final future = sut.auth(params);
+    expect(future, throwsA(DomainError.unexpected));
+  });
+
+  test('Should throw unexpected if HttpClient return 404 ', () {
+    when(httpClient.request(url: url, method: "post", body: params.toJson()))
+        .thenThrow(HttpError.notFound);
+
+    final future = sut.auth(params);
+    expect(future, throwsA(DomainError.unexpected));
+  });
+
+  test('Should throw unexpected if HttpClient return 500 ', () {
+    when(httpClient.request(url: url, method: "post", body: params.toJson()))
+        .thenThrow(HttpError.serverError);
+
+    final future = sut.auth(params);
+    expect(future, throwsA(DomainError.unexpected));
+  });
+
+  test('Should throw InvalidCredentialsError if HttpClient return 401 ', () {
+    when(httpClient.request(url: url, method: "post", body: params.toJson()))
+        .thenThrow(HttpError.unauthorized);
+
+    final future = sut.auth(params);
+    expect(future, throwsA(DomainError.invalidCredentials));
   });
 }
